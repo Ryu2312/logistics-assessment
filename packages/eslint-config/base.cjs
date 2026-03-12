046ef44adb8298ba2e0a27ae7487fbd1e7853ef6
Ryu <alessiasophia23@gmail.com>
2026-03-12 00:18:40 -0500

    chore: add pnpm workspace configuration and Turbo build setup
    - Created pnpm-workspace.yaml to define package structure for pnpm.
    - Added turbo.json to configure Turbo tasks for build, lint, type checking, and development.

---
+++ packages/eslint-config/base.cjs
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "import", "unused-imports"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  rules: {
+    eqeqeq: ["error", "smart"],
+    "prefer-const": "error",
+    "no-implicit-globals": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
